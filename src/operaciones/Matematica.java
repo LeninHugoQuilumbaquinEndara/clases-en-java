4c7db20a00ef379a73e0edc35211e2e5f6ca6893
LENIN QUILUMBAQUIN <lhquilumbaquin@espe.edu.ec>
2021-06-02 10:44:22 -0500

    v1 clase matematica

---
+++ src/operaciones/Matematica.java
@@ -0,0 +1,11 @@
+package operaciones;
+
+
+public class Matematica {
+    int a;
+    int b;
+    public void sumar(){
+        int resultado= a+b;
+        System.out.println("Su resultado es "+resultado);
+    }
+}
